b7854a630237f9c70a071c17adc012da24bb795a
Martin Atkins <matkins@saymedia.com>
2015-10-02 17:41:19 -0700

    Allow multiple service names to be specified.
    Some projects will include multiple distinct services. This allows the
    union of all of the service nodes to be included in the resource model,
    with tags to differentiate them where needed in job definitions.

--- main.go
+++ main.go
@@ -25,12 +25,12 @@ type Node struct {
 
 func main() {
 
-	if len(os.Args) != 2 {
-		fmt.Fprintf(os.Stderr, "Usage: %s <service-name>\n", os.Args[0])
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s service-names...\n", os.Args[0])
 		os.Exit(1)
 	}
 
-	serviceName := os.Args[1]
+	serviceNames := os.Args[1:]
 	consulAddr := os.Getenv("CONSUL_ADDRESS")
 	consulScheme := os.Getenv("CONSUL_SCHEME")
 	consulToken := os.Getenv("CONSUL_TOKEN")
@@ -48,14 +48,14 @@ func main() {
 		Token:   consulToken,
 	}
 
-	err := Generate(config, serviceName)
+	err := Generate(config, serviceNames)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
 		os.Exit(2)
 	}
 }
 
-func Generate(config *consul.Config, serviceName string) error {
+func Generate(config *consul.Config, serviceNames []string) error {
 	client, err := consul.NewClient(config)
 	if err != nil {
 		return err
@@ -72,25 +72,28 @@ func Generate(config *consul.Config, serviceName string) error {
 	options := &consul.QueryOptions{}
 	for _, dc := range datacenters {
 		options.Datacenter = dc
-		endpoints, _, err := catalog.Service(serviceName, "", options)
-		if err != nil {
-			return err
-		}
-
 		addressTags := make(map[string]map[string]bool)
-		for _, endpoint := range endpoints {
-			address := endpoint.Address
-			if _, ok := addressTags[address]; !ok {
-				addressTags[address] = make(map[string]bool)
+		for _, serviceName := range serviceNames {
+			endpoints, _, err := catalog.Service(serviceName, "", options)
+			if err != nil {
+				return err
 			}
-			for _, tag := range endpoint.ServiceTags {
-				addressTags[address][tag] = true
+
+			for _, endpoint := range endpoints {
+				address := endpoint.Address
+				if _, ok := addressTags[address]; !ok {
+					addressTags[address] = make(map[string]bool)
+				}
+				for _, tag := range endpoint.ServiceTags {
+					addressTags[address][tag] = true
+				}
+				// Add an extra "virtual" tag for the service name.
+				addressTags[address][serviceName] = true
 			}
 		}
 
 		for address, tagsMap := range addressTags {
-			tags := make([]string, 0, len(tagsMap)+1)
-			tags = append(tags, serviceName)
+			tags := make([]string, 0, len(tagsMap))
 			for tag, _ := range tagsMap {
 				tags = append(tags, tag)
 			}
