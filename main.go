d75a51b4ac8823ebbf9e0ca2bffe5022bb05ef33
Martin Atkins <matkins@saymedia.com>
2015-10-02 17:31:45 -0700

    Basic initial implementation.
    Writes out a simple resource XML that includes all of the nodes running
    a particular given service. Currently assumes that the login on all hosts
    is "ubuntu", so that'll need to be addressed in a later change.

--- main.go
+++ main.go
@@ -1,9 +1,117 @@
 package main
 
 import (
-    consul "github.com/hashicorp/consul/api"
+	"encoding/xml"
+	"fmt"
+	"os"
+	"strings"
+
+	consul "github.com/hashicorp/consul/api"
 )
+
+type Project struct {
+	XMLName xml.Name `xml:"project"`
+	Nodes   []Node
+}
+
+type Node struct {
+	XMLName        xml.Name `xml:"node"`
+	Name           string   `xml:"name,attr"`
+	Hostname       string   `xml:"hostname,att"`
+	Tags           string   `xml:"tags,attr,omitempty"`
+	Username       string   `xml:"username,attr"`
+	DatacenterName string   `xml:"datacenter,attr"`
+}
 
 func main() {
 
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <service-name>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	serviceName := os.Args[1]
+	consulAddr := os.Getenv("CONSUL_ADDRESS")
+	consulScheme := os.Getenv("CONSUL_SCHEME")
+	consulToken := os.Getenv("CONSUL_TOKEN")
+
+	if consulAddr == "" {
+		consulAddr = "127.0.0.1:8500"
+	}
+	if consulScheme == "" {
+		consulScheme = "http"
+	}
+
+	config := &consul.Config{
+		Address: consulAddr,
+		Scheme:  consulScheme,
+		Token:   consulToken,
+	}
+
+	err := Generate(config, serviceName)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		os.Exit(2)
+	}
+}
+
+func Generate(config *consul.Config, serviceName string) error {
+	client, err := consul.NewClient(config)
+	if err != nil {
+		return err
+	}
+	catalog := client.Catalog()
+
+	datacenters, err := catalog.Datacenters()
+	if err != nil {
+		return err
+	}
+
+	project := &Project{}
+
+	options := &consul.QueryOptions{}
+	for _, dc := range datacenters {
+		options.Datacenter = dc
+		endpoints, _, err := catalog.Service(serviceName, "", options)
+		if err != nil {
+			return err
+		}
+
+		addressTags := make(map[string]map[string]bool)
+		for _, endpoint := range endpoints {
+			address := endpoint.Address
+			if _, ok := addressTags[address]; !ok {
+				addressTags[address] = make(map[string]bool)
+			}
+			for _, tag := range endpoint.ServiceTags {
+				addressTags[address][tag] = true
+			}
+		}
+
+		for address, tagsMap := range addressTags {
+			tags := make([]string, 0, len(tagsMap))
+			for tag, _ := range tagsMap {
+				tags = append(tags, tag)
+			}
+			node := Node{
+				Name:     address,
+				Hostname: address,
+				// TODO: Make username configurable
+				Username:       "ubuntu",
+				DatacenterName: dc,
+				Tags:           strings.Join(tags, ","),
+			}
+			project.Nodes = append(project.Nodes, node)
+		}
+	}
+
+	xmlBytes, err := xml.MarshalIndent(project, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	os.Stdout.Write(xmlBytes)
+	os.Stdout.Write([]byte{'\n'})
+
+	return nil
 }
